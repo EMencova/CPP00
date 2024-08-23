5acab5ca60d9bdec8d54c4bcfacaf0bf5401cdad
Eliska Mencova <emencova@Eliskas-MacBook-Pro.local>
2024-08-23 21:34:32 +0100

    phonebook

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -1,42 +1,95 @@
 
 #include "phonebook.hpp"
 
+void searchContacts(PhoneBook &PPBB)
+{
+    system("clear");
+
+    if (PPBB.nbr == 0) {
+        cout << "0 contacts." << endl;
+        return;
+    }
+
+    cout << setw(10) << right << "Index" << " | "
+         << setw(10) << right << "First Name" << " | "
+         << setw(10) << right << "Last Name" << " | "
+         << setw(10) << right << "Nick Name" << " |" << endl;
+
+    for (int i = 0; i < PPBB.nbr; i++) {
+        PPBB.Contacts[i].printContact(i);
+    }
+
+    int index;
+    cout << "Enter the index number to view the contact details: ";
+
+    while (!(cin >> index))
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Invalid input! Please enter a valid number: ";
+    }
+
+    if (index > 0 && index <= PPBB.nbr) {
+        system("clear");
+        PPBB.Contacts[index - 1].displayFullContact();
+    } else {
+        cout << "Invalid index!" << endl;
+    }
+
+    cout << "Press any key to return to the menu." << endl;
+    cin.ignore();
+    cin.get();
+}
+
 
 int main()
 {
-    PhoneBook PB;
+    PhoneBook PPBB;
     string cmnd;
-    PhoneBook.nbr = 1;
-    
-    while (1)
+    string input;
+
+    while (1) 
     {
         system("clear");
-        cout<<" ADD - 1 SEARCH - 2 EXIT - 3 " << endl;
-        cin>> cmnd;
-        
-        if (cmnd == "ADD" || cmnd == "1")
+        cout << "ADD - 1 | SEARCH - 2 | EXIT - 3" << endl;
+        cin >> cmnd;
+
+        if (cmnd == "ADD" || cmnd == "1") 
         {
             system("clear");
             cout << "Enter your first name: ";
-            cin>>PB.FirstName;
-            cout<< "Enter your last name: ";
-            cin>>PB.LastName;
-            cout<< "Enter your nickname: ";
-            cin>>PB.NickName;
-            cout<< "Enter your phone number: ";
-            cin>>PB.PhoneNumber;
-            cout<< "Enter your darkest secret: ";
-            cin>>PB.Darkest_Secret;
+            cin >> input;
+            PPBB.Contacts[PPBB.current].setFirstName(input);
+
+            cout << "Enter your last name: ";
+            cin >> input;
+            PPBB.Contacts[PPBB.current].setLastName(input);
+
+            cout << "Enter your nickname: ";
+            cin >> input;
+            PPBB.Contacts[PPBB.current].setNickName(input);
+
+            cout << "Enter your phone number: ";
+            cin >> input;
+            PPBB.Contacts[PPBB.current].setPhoneNumber(input);
+
+            cout << "Enter your darkest secret: ";
+            cin >> input;
+            PPBB.Contacts[PPBB.current].setDarkestSecret(input);
+
+            PPBB.current = (PPBB.current + 1) % 8;
+
+            if (PPBB.nbr < 8)
+                PPBB.nbr++;
+            
         }
-        if (cmnd == "SEARCH" || cmnd == "2")
-        {
-            system("clear");
-            cout<< PB.Contacts << " | " ;
-            cout<<"First name: "<<PB.FirstName
-            cout<<" | " ;
-            cout<<"Last name: "<<PB.LastName
-            cout<< " | " ;
-            cout<< "Nick name" <<PB.Nickname
+        else if (cmnd == "SEARCH" || cmnd == "2") {
+            searchContacts(PPBB);
+        }
+        else if (cmnd == "EXIT" || cmnd == "3") {
+            break;
         }
     }
+
+    return 0;
 }
--- ex01/phonebook.hpp
+++ ex01/phonebook.hpp
@@ -15,11 +15,12 @@
 
 # include <iostream>
 # include <string>
-//# include <iomanip>
+# include <iomanip>
+# include <cstdlib>
 
 using	namespace std;
 
-class	contact
+class	Contact
 {
 public:
     string FirstName;
@@ -28,42 +29,71 @@ public:
     string PhoneNumber;
     string Darkest_Secret;
     
-   /* void createContact()
-     {
-     cout << "Enter your first name: ";
-     cin>>PB.FirstName;
-     
-     cout<< "Enter your last name: ";
-     cin>>PB.LastName;
-     
-     cout<< "Enter your nickname: ";
-     cin>>PB.NickName;
-     
-     cout<< "Enter your phone number: ";
-     cin>>PB.PhoneNumber;
-     
-     cout<< "Enter your darkest secret: ";
-     cin>>PB.Darkest_Secret;
-    
-    void showContact()
+    string formatString(const string str)
     {
-        cout<< PB.Contacts << " | " ;
-        cout<<"First name: "<<FirstName
-        cout<<" | " ;
-        cout<<"Last name: "<<LastName
-        cout<< " | " ;
-        cout<< "Nick name" <<Nickname
-        cout<< " | " ;
+        if (str.size() > 10)
+            return str.substr(0, 9) + ".";
+        else
+            return str;
     }
-    */
+    void printContact(int index)
+    {
+        Contact PB;
+        cout << setw(10) << right << index + 1 << " | "
+             << setw(10) << right << formatString(FirstName) << " | "
+             << setw(10) << right << formatString(LastName) << " | "
+             << setw(10) << right << formatString(NickName) << " |" << endl;
+    }
+
+    void displayFullContact()
+    {
+        Contact PB;
+        cout << "First name: " << FirstName << endl;
+        cout << "Last name: " << LastName << endl;
+        cout << "Nick name: " << NickName << endl;
+        cout << "Phone number: " << PhoneNumber << endl;
+        cout << "Darkest secret: " << Darkest_Secret << endl;
+    }
+    
+    void setFirstName(const string& firstName)
+    {
+        FirstName = firstName;
+    }
+
+    void setLastName(const string& lastName)
+    {
+        LastName = lastName;
+    }
+
+    void setNickName(const string& nickName)
+    {
+        NickName = nickName;
+    }
+
+    void setPhoneNumber(const string& phoneNumber)
+    {
+        PhoneNumber = phoneNumber;
+    }
+
+    void setDarkestSecret(const string& darkestSecret)
+    {
+        Darkest_Secret = darkestSecret;
+    }
     
 };
 
 class PhoneBook
 {
-    public:
-        Contacts[8];
-    int     nbr;
+public:
+    Contact Contacts[8];
+    int     nbr ;
+    int     current;
+    
+    PhoneBook()
+    {
+        current = 0;
+        nbr = 0;
+    }
 };
 
 #endif
