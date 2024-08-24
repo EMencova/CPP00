c2edd3a0c87abc34a293467f7ba3a19d54c07516
Eliska Mencova <emencova@Eliskas-MacBook-Pro.local>
2024-08-24 14:05:03 +0100

    corrected

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -1,80 +1,80 @@
 
-#include "phonebook.hpp"
+#include "PhoneBook.hpp"
 
 void searchContacts(PhoneBook &PPBB)
 {
     system("clear");
 
     if (PPBB.nbr == 0) {
-        cout << "0 contacts." << endl;
+        std::cout << "0 contacts." << std::endl;
         return;
     }
-
-    cout << setw(10) << right << "Index" << " | "
-         << setw(10) << right << "First Name" << " | "
-         << setw(10) << right << "Last Name" << " | "
-         << setw(10) << right << "Nick Name" << " |" << endl;
+    
+    std::cout << std::setw(10) << std::right << "Index" << " | "
+             << std::setw(10) << std::right << "First Name" << " | "
+             << std::setw(10) << std::right << "Last Name" << " | "
+             << std::setw(10) << std::right << "Nick Name" << " |" << std::endl;
 
     for (int i = 0; i < PPBB.nbr; i++) {
         PPBB.Contacts[i].printContact(i);
     }
 
     int index;
-    cout << "Enter the index number to view the contact details: ";
+    std::cout << "Enter the index number to view the contact details: ";
 
-    while (!(cin >> index))
+    while (!(std::cin >> index))
     {
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        cout << "Invalid input! Please enter a valid number: ";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Invalid input! Please enter a valid number: ";
     }
 
     if (index > 0 && index <= PPBB.nbr) {
         system("clear");
         PPBB.Contacts[index - 1].displayFullContact();
     } else {
-        cout << "Invalid index!" << endl;
+        std::cout << "Invalid index!" << std::endl;
     }
 
-    cout << "Press any key to return to the menu." << endl;
-    cin.ignore();
-    cin.get();
+    std::cout << "Press any key to return to the menu." << std::endl;
+    std::cin.ignore();
+    std::cin.get();
 }
 
 
 int main()
 {
     PhoneBook PPBB;
-    string cmnd;
-    string input;
+    std::string cmnd;
+    std::string input;
 
     while (1) 
     {
         system("clear");
-        cout << "ADD - 1 | SEARCH - 2 | EXIT - 3" << endl;
-        cin >> cmnd;
+        std::cout << "ADD - 1 | SEARCH - 2 | EXIT - 3" << std::endl;
+        std::cin >> cmnd;
 
         if (cmnd == "ADD" || cmnd == "1") 
         {
             system("clear");
-            cout << "Enter your first name: ";
-            cin >> input;
+            std::cout << "Enter your first name: ";
+            std::cin >> input;
             PPBB.Contacts[PPBB.current].setFirstName(input);
 
-            cout << "Enter your last name: ";
-            cin >> input;
+            std::cout << "Enter your last name: ";
+            std::cin >> input;
             PPBB.Contacts[PPBB.current].setLastName(input);
 
-            cout << "Enter your nickname: ";
-            cin >> input;
+            std::cout << "Enter your nickname: ";
+            std::cin >> input;
             PPBB.Contacts[PPBB.current].setNickName(input);
 
-            cout << "Enter your phone number: ";
-            cin >> input;
+            std::cout << "Enter your phone number: ";
+            std::cin >> input;
             PPBB.Contacts[PPBB.current].setPhoneNumber(input);
 
-            cout << "Enter your darkest secret: ";
-            cin >> input;
+            std::cout << "Enter your darkest secret: ";
+            std::cin >> input;
             PPBB.Contacts[PPBB.current].setDarkestSecret(input);
 
             PPBB.current = (PPBB.current + 1) % 8;
--- ex01/phonebook.hpp
+++ ex01/phonebook.hpp
@@ -18,64 +18,62 @@
 # include <iomanip>
 # include <cstdlib>
 
-using	namespace std;
-
 class	Contact
 {
 public:
-    string FirstName;
-    string LastName;
-    string NickName;
-    string PhoneNumber;
-    string Darkest_Secret;
+    std::string FirstName;
+    std::string LastName;
+    std::string NickName;
+    std::string PhoneNumber;
+    std::string Darkest_Secret;
     
-    string formatString(const string str)
+    std::string formatString(const std::string str)
     {
         if (str.size() > 10)
             return str.substr(0, 9) + ".";
         else
             return str;
     }
-    void printContact(int index)
+   void printContact(int index)
     {
         Contact PB;
-        cout << setw(10) << right << index + 1 << " | "
-             << setw(10) << right << formatString(FirstName) << " | "
-             << setw(10) << right << formatString(LastName) << " | "
-             << setw(10) << right << formatString(NickName) << " |" << endl;
+        std::cout << std::setw(10) << std::right << index + 1 << " | "
+             << std::setw(10) << std::right << formatString(FirstName) << " | "
+             << std::setw(10) << std::right << formatString(LastName) << " | "
+             << std::setw(10) << std::right << formatString(NickName) << " |" << std::endl;
     }
 
     void displayFullContact()
     {
         Contact PB;
-        cout << "First name: " << FirstName << endl;
-        cout << "Last name: " << LastName << endl;
-        cout << "Nick name: " << NickName << endl;
-        cout << "Phone number: " << PhoneNumber << endl;
-        cout << "Darkest secret: " << Darkest_Secret << endl;
+        std::cout << "First name: " << FirstName << std::endl;
+        std::cout << "Last name: " << LastName << std::endl;
+        std::cout << "Nick name: " << NickName << std::endl;
+        std::cout << "Phone number: " << PhoneNumber << std::endl;
+        std::cout << "Darkest secret: " << Darkest_Secret << std::endl;
     }
     
-    void setFirstName(const string& firstName)
+    void setFirstName(const std::string& firstName)
     {
         FirstName = firstName;
     }
 
-    void setLastName(const string& lastName)
+    void setLastName(const std::string& lastName)
     {
         LastName = lastName;
     }
 
-    void setNickName(const string& nickName)
+    void setNickName(const std::string& nickName)
     {
         NickName = nickName;
     }
 
-    void setPhoneNumber(const string& phoneNumber)
+    void setPhoneNumber(const std::string& phoneNumber)
     {
         PhoneNumber = phoneNumber;
     }
 
-    void setDarkestSecret(const string& darkestSecret)
+    void setDarkestSecret(const std::string& darkestSecret)
     {
         Darkest_Secret = darkestSecret;
     }
